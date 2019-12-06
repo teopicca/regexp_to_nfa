62e6444f2b8621a26fd4c7322a94a53c107188a3
Daniele Potertì <38102850+danielepoterti@users.noreply.github.com>
2019-12-06 18:22:45 +0100

    nfa_test prototype

---
+++ prolog/nfa_test.pl
@@ -0,0 +1,19 @@
+nfa_test(FA_Id, In):-
+    nfa_initial(FA_Id, S),
+    nfa_test_accept(FA_Id, In, S).
+
+nfa_test_accept(FA_Id, [], Q):-
+    nfa_final(FA_Id, Q),
+    !.
+    
+
+nfa_test_accept(FA_Id, [In | Ins], S):-
+    nfa_delta(FA_Id, S, In, N),
+    nfa_test_accept(FA_Id, Ins, N),
+    !.
+
+nfa_test_accept(FA_Id, [In | Ins], S):-
+    nfa_delta(FA_Id, S, ¦Å, N),
+    nfa_test_accept(FA_Id, Ins, N),
+    !.
+
