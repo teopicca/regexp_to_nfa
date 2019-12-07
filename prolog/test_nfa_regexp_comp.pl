c6d29e040d0e48dd5c5874fcd9bcfe3b6cd66572
LabberDev <emruocco2@gmail.com>
2019-12-07 16:04:01 +0100

    Bux fix
    Ho modificato il caso base e sistemato la seq e altri errori.

--- prolog/test_nfa_regexp_comp.pl
+++ prolog/test_nfa_regexp_comp.pl
@@ -1,93 +1,76 @@
 %%%% -*- Mode: Prolog -*-
-%%%% isregexp.pl
+%%%% test_nfa_regexp_comp.pl
 
 nfa_regexp_comp(FA_Id, RE):-
-    is_regexp(RE),
+%    is_regexp(RE),
     nonvar(FA_Id),
     RE =.. [OP | Args],
     regexp_comp(FA_Id, OP, Args,QIn, QF),
     asserta(nfa_final(FA_Id, QF)),
     asserta(nfa_initial(FA_Id, QIn)).
 
-regexp_comp(FA_Id, or, [ Arg | Args], QIn, QF):-
-    atomic(Arg),
-    regexp_comp(FA_Id, atomic, Arg),
-    regexp_comp(FA_Id, or, Args).
-
-%% caso ricorsivo 'or' 
+%% caso ricorsivo 'or'
 regexp_comp(FA_Id, or, [Arg | Args], QIn, QF) :-
     Args \= [],
     regexp_comp(FA_Id, or, Args, QIn, QF),
-    caso_base(FA_Id, Arg, QIn2, QF2),
-    assert(nfa_delta(FA_Id, QIn, ε, QIn2)),
-    assert(nfa_delta(FA_Id, QF2, ε, QF)).
+    scompatta(FA_Id, [Arg], QIn2, QF2),
+    asserta(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
+    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)).
 
 %% caso base 'or'
-regexp_comp(FA_Id, or, [Arg | []], QIn, QF) :-
-    caso_base(FA_Id, Arg, QIn2, QF2), 
-    gensym(q, QIn),
-    gensym(q, QF), 
-    assert(nfa_delta(FA_Id, QIn, ε, QIn2)), 
-    assert(nfa_delta(FA_Id, QF2, ε, QF)).
-
-%% caso 'star'    
-regexp_comp(FA_Id, star, Arg, QIn, QF) :-
-    caso_base(FA_Id, Arg, QIn2, QF2),
+regexp_comp(FA_Id, or, Arg, QIn, QF) :-
+    scompatta(FA_Id, Arg, QIn2, QF2),
     gensym(q, QIn),
     gensym(q, QF),
-    assert(nfa_delta(FA_Id, QIn, ε, QIn2)),
-    assert(nfa_delta(FA_Id, QIn, ε, QF)),
-    assert(nfa_delta(FA_Id, QF2, ε, QIn2)),
-    assert(nfa_delta(FA_Id, QF2, ε, QF)).
+    asserta(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
+    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)).
 
-%% caso 'plus'
+%% caso 'star'
+regexp_comp(FA_Id, star, Arg, QIn, QF) :-
+    scompatta(FA_Id, Arg, QIn2, QF2),
+    gensym(q, QIn),
+    gensym(q, QF),
+    asserta(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
+    asserta(nfa_delta(FA_Id, QIn, epsilon, QF)),
+    asserta(nfa_delta(FA_Id, QF2, epsilon, QIn2)),
+    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)).
+
+%% caso 'plus' in questo caso vengono creati anche uno stato iniziale e
+%% uno finale
 regexp_comp(FA_Id, plus, Arg, QIn, QF) :-
-    caso_base(FA_Id, Arg, QIn2, QF2),
+    scompatta(FA_Id, Arg, QIn2, QF2),
     gensym(q, QIn),
     gensym(q, QF),
     regexp_comp(FA_Id, star, Arg, QIn3, QF3),
-    assert(nfa_delta(FA_Id, QIn, ε, QIn2)),
-    assert(nfa_delta(FA_Id, QF2, ε, QIn3)),
-    assert(nfa_delta(FA_Id, QF3, ε, QF)).
+    asserta(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
+    asserta(nfa_delta(FA_Id, QF2, epsilon, QIn3)),
+    asserta(nfa_delta(FA_Id, QF3, epsilon, QF)).
 
-%% caso 'seq'
-regexp_comp(Fa_Id, seq, Arg, QIn, QF) :-
-    caso_base(FA_Id, Arg, QIn2, QF2),
-    caso_base(FA_Id, Arg, QIn3, QF3),
+%% caso iniziale 'seq'
+regexp_comp(FA_Id, seq, Arg, QIn, QF) :-
+    regexp_comp_rec(FA_Id, seq, Arg, QIn2, QF2),
     gensym(q, QIn),
     gensym(q, QF),
-    assert(nfa_delta(FA_Id, QIn, ε, QIn2)),
-    assert(nfa_delta(FA_Id, QF2, ε, QIn3)),
-    assert(nfa_delta(FA_Id, QF3, ε, QF)).  
+    assert(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
+    assert(nfa_delta(FA_Id, QF2, epsilon, QF)).
 
-
-%% Controlla se il caso base passato è un caso finale oppure un caso ricorsivo (or, star, plus seq)
-caso_base(FA_Id, Arg, QIn, QF) :-
-    Arg =.. [Op | Args], 
-    caso_base_scelta(FA_Id, Op, Args, QIn, QF).
-
-%% Se il caso base è un 'or', richiama la funzione corretta
-caso_base_scelta(FA_Id, or, Args, QIn, QF) :-
-    Args \= [],
-    regexp_comp(FA_Id, or, Args, QIn, QF).
-
-%% Se il caso base è una 'star', richiama la funzione corretta
-caso_base_scelta(FA_Id, star, Args, QIn, QF) :-
-    Args \= [],
-    regexp_comp(FA_Id, star, Args, QIn, QF).
-
-%% Se il caso base è una 'plus', richiama la funzione corretta
-caso_base_scelta(FA_Id, plus, Args, QIn, QF) :-
-    Args \= [],
-    regexp_comp(FA_Id, plus, Args, QIn, QF).
-    
-%% Se il caso base è una 'seq', richiama la funzione corretta
-caso_base_scelta(FA_Id, seq, Args, QIn, QF) :-
-    Args \= [],
-    regexp_comp(FA_Id, seq, Args, QIn, QF).
-
-%% Quando Arg nel predicato caso_base è un atomo, viene eseguito questo predicato (creazione di un singolo nodo).
-caso_base_scelta(FA_Id, Op, [], QIn, QF) :-
+%%  caso base
+regexp_comp(FA_Id, Op, [], QIn, QF) :-
     gensym(q, QIn),
     gensym(q, QF),
     assert(nfa_delta(FA_Id, QIn, Op, QF)).
+
+%% caso ricorsivo 'seq'
+regexp_comp_rec(FA_Id, seq, [Arg | Args], QIn, QF2) :-
+    Args \= [],
+    scompatta(FA_Id, [Arg], QIn, QF3),
+    regexp_comp_rec(FA_Id, seq, Args, QIn2, QF2),
+    assert(nfa_delta(FA_Id, QF3, epsilon, QIn2)).
+
+%%  caso base 'seq'
+regexp_comp_rec(FA_Id, seq, [Arg], QIn, QF) :-
+    scompatta(FA_Id, [Arg], QIn, QF).
+
+scompatta(FA_Id, [Arg], QIn, QF) :-
+    Arg =.. [Op | Args],
+    regexp_comp(FA_Id, Op, Args, QIn, QF).
