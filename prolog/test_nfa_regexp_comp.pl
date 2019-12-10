8e93215413148dba7c3c1c05e83ccc9fcf6bacbd
Daniele Potertì <38102850+danielepoterti@users.noreply.github.com>
2019-12-10 21:40:27 +0100

    compound base case added

--- prolog/test_nfa_regexp_comp.pl
+++ prolog/test_nfa_regexp_comp.pl
@@ -1,37 +1,14 @@
 %%%% -*- Mode: Prolog -*-
 %%%% test_nfa_regexp_comp.pl
 
-
 nfa_regexp_comp(FA_Id, RE):-
-%    is_regexp(RE),
+    is_regexp(RE),
     nonvar(FA_Id),
+    not(nfa_initial(FA_Id, _)),
     RE =.. [OP | Args],
-    regexp_comp(FA_Id, OP, Args, QIn, QF),
+    regexp_comp(FA_Id, OP, Args,QIn, QF),
     asserta(nfa_final(FA_Id, QF)),
     asserta(nfa_initial(FA_Id, QIn)).
-
-nfa_regexp_comp(FA_Id, RE):-
-%    is_regexp(RE),
-    nonvar(FA_Id),
-    RE =.. [OP | [Args]],
-    compound(Args),
-    term_to_atom(Args, A),
-    regexp_comp(FA_Id, OP, [A], QIn, QF),
-    asserta(nfa_final(FA_Id, QF)),
-    asserta(nfa_initial(FA_Id, QIn)).
-
-nfa_regexp_comp(FA_Id, RE):-
-%    is_regexp(RE),
-    nonvar(FA_Id),
-    compound(RE),
-    term_to_atom(Args, A),
-    regexp_comp(FA_Id, OP, [A], QIn, QF),
-    asserta(nfa_final(FA_Id, QF)),
-    asserta(nfa_initial(FA_Id, QIn)).
-
-
-
-
 
 %% caso ricorsivo 'or'
 regexp_comp(FA_Id, or, [Arg | Args], QIn, QF) :-
@@ -39,7 +16,8 @@ regexp_comp(FA_Id, or, [Arg | Args], QIn, QF) :-
     regexp_comp(FA_Id, or, Args, QIn, QF),
     scompatta(FA_Id, [Arg], QIn2, QF2),
     asserta(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
-    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)).
+    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)),
+    !.
 
 %% caso base 'or'
 regexp_comp(FA_Id, or, Arg, QIn, QF) :-
@@ -47,7 +25,8 @@ regexp_comp(FA_Id, or, Arg, QIn, QF) :-
     gensym(q, QIn),
     gensym(q, QF),
     asserta(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
-    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)).
+    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)),
+    !.
 
 %% caso 'star'
 regexp_comp(FA_Id, star, Arg, QIn, QF) :-
@@ -57,7 +36,8 @@ regexp_comp(FA_Id, star, Arg, QIn, QF) :-
     asserta(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
     asserta(nfa_delta(FA_Id, QIn, epsilon, QF)),
     asserta(nfa_delta(FA_Id, QF2, epsilon, QIn2)),
-    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)).
+    asserta(nfa_delta(FA_Id, QF2, epsilon, QF)),
+    !.
 
 %% caso 'plus' in questo caso vengono creati anche uno stato iniziale e
 %% uno finale
@@ -68,7 +48,8 @@ regexp_comp(FA_Id, plus, Arg, QIn, QF) :-
     regexp_comp(FA_Id, star, Arg, QIn3, QF3),
     asserta(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
     asserta(nfa_delta(FA_Id, QF2, epsilon, QIn3)),
-    asserta(nfa_delta(FA_Id, QF3, epsilon, QF)).
+    asserta(nfa_delta(FA_Id, QF3, epsilon, QF)),
+    !.
 
 %% caso iniziale 'seq'
 regexp_comp(FA_Id, seq, Arg, QIn, QF) :-
@@ -76,13 +57,23 @@ regexp_comp(FA_Id, seq, Arg, QIn, QF) :-
     gensym(q, QIn),
     gensym(q, QF),
     assert(nfa_delta(FA_Id, QIn, epsilon, QIn2)),
-    assert(nfa_delta(FA_Id, QF2, epsilon, QF)).
+    assert(nfa_delta(FA_Id, QF2, epsilon, QF)),
+    !.
 
 %%  caso base
 regexp_comp(FA_Id, Op, [], QIn, QF) :-
     gensym(q, QIn),
     gensym(q, QF),
-    assert(nfa_delta(FA_Id, QIn, Op, QF)).
+    assert(nfa_delta(FA_Id, QIn, Op, QF)),
+    !.
+
+regexp_comp(FA_Id, X, Xs, QIn, QF) :-
+    Arg =.. [X | Xs],
+    gensym(q, QIn),
+    gensym(q, QF),
+    assert(nfa_delta(FA_Id, QIn, Arg, QF)),
+    !.
+    
 
 %% caso ricorsivo 'seq'
 regexp_comp_rec(FA_Id, seq, [Arg | Args], QIn, QF2) :-
@@ -99,13 +90,3 @@ regexp_comp_rec(FA_Id, seq, [Arg], QIn, QF) :-
 scompatta(FA_Id, [Arg], QIn, QF) :-
     Arg =.. [Op | Args],
     regexp_comp(FA_Id, Op, Args, QIn, QF).
-
-
-
-
-    
-    
-
-
-    
-    
