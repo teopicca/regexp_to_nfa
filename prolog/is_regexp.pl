b7ec042389337e743ba818f2765cd41bd142f27a
Daniele Potertì <38102850+danielepoterti@users.noreply.github.com>
2019-12-13 12:30:56 +0100

    a() case is now accepted

--- prolog/is_regexp.pl
+++ prolog/is_regexp.pl
@@ -1,60 +1,117 @@
 %%%% -*- Mode: Prolog -*-
+
 %%%% is_regexp.pl
 
+
+
     
+
 operator(star).
+
 operator(plus).
+
 operator(or).
+
 operator(seq).
 
 
+
+
+
 not_op(Goal) :- call(Goal), !, fail.
+
 not_op(_).
 
+
+
 is_regexp([star | Xs]):-
+
     length(Xs, Y),
+
     !,
+
     Y == 1,
+
     is_regexp(Xs).
+
+
 
 is_regexp([plus | Xs]):-
+
     length(Xs, Y),
+
     !,
+
     Y == 1,
+
     is_regexp(Xs).
 
+
+
 is_regexp([or | Xs]):-
+
     length(Xs, Y),
+
     !,
+
     Y >=2,
+
     is_regexp(Xs). 
 
+
+
 is_regexp([seq | Xs]):-
+
     length(Xs, Y),
+
     !,
+
     Y >=2,
+
     is_regexp(Xs). 
+
+
+
 
 
 is_regexp([X|Xs]):-
+
     is_regexp(X),
+
     is_regexp(Xs),
+
     !.
+
+
 
 is_regexp([]):- !.
 
-is_regexp(X):-
-    atomic(X),
-    !.
+
 
 is_regexp(X):-
-    compound(X),
-  % arg(_, X, _), TODO: riconoscere a()
-    functor(X, F, _),
-    not_op(operator(F)),
+
+    atomic(X),
+
     !.
+
+
+
+is_regexp(X):-
+
+    compound(X),
+
+    compound_name_arity(X, F, _),
+
+    not_op(operator(F)),
+
+    !.
+
+
 
 is_regexp(RE):-
+
     RE =.. REList,
+
     is_regexp(REList),
+
     !.
