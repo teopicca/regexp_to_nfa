86939e5934fa43581ff6f7f62126f5b6909cfebd
Daniele Potertì <38102850+danielepoterti@users.noreply.github.com>
2019-12-22 12:27:23 +0100

    (is-regexp '(star or)) is now accepted

--- lisp/is-regexp.lisp
+++ lisp/is-regexp.lisp
@@ -1,32 +1,34 @@
 (defun is-regexp (RE)
   (if (atom RE)
       T
-    (cond
+    (if (null RE) 
+        NIL
 
-     ((null RE) 
-      NIL)
+      (if (or (equalp (car RE) 'star) (equalp (car RE) 'plus))
+          (if (equalp (list-length (cdr RE)) 1)
+              (is-regexp-body (cdr RE))
+            NIL)
 
-     
+        (if (or (equalp (car RE) 'seq)(equalp (car RE) 'or))
+            (if (>= (list-length (cdr RE)) 2)
+                (is-regexp-body-mul (cdr RE))
+              NIL)
 
-     ((or (equalp (car RE) 'star) (equalp (car RE) 'plus))
-      (if (equalp (list-length (cdr RE)) 1)
-          (is-regexp (cdr RE))
-        NIL))
-
-     ((or (equalp (car RE) 'seq)(equalp (car RE) 'or))
-      (if (>= (list-length (cdr RE)) 2)
-          (is-regexp-body-mul (cdr RE))
-        NIL))
-
-     ((atom (car RE)) 
-      T)
-
-     ((listp (car RE)) 
-      T))))
+          (if (atom (car RE)) 
+              T
+            (if (listp (car RE)) 
+                T)))))))
 
 
 (defun is-regexp-body-mul (RE)
 (if (not (null (cdr RE)))
-    (and (is-regexp (car RE))
+    (and (is-regexp-body (car RE))
          (is-regexp-body-mul (cdr RE)))
   (is-regexp (car RE))))
+
+(defun is-regexp-body (RE)
+(if (atom RE)
+    T
+  (if (listp (car RE))
+      (is-regexp (car RE))
+    T)))
