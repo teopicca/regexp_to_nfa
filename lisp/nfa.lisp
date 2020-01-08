fd076e4b2faabc55b60d9490476770356f079b42
picca <matteopiccapietra@gmail.com>
2020-01-08 13:20:37 +0100

    code formatting

--- lisp/nfa.lisp
+++ lisp/nfa.lisp
@@ -7,22 +7,18 @@
       T
     (if (null RE) 
         NIL
-
       (if (or (equalp (car RE) 'star) (equalp (car RE) 'plus))
           (if (equalp (list-length (cdr RE)) 1)
               (is-regexp-body (cdr RE))
             NIL)
-
         (if (or (equalp (car RE) 'seq)(equalp (car RE) 'or))
             (if (>= (list-length (cdr RE)) 2)
                 (is-regexp-body-mul (cdr RE))
               NIL)
-
           (if (atom (car RE)) 
               T
             (if (listp (car RE)) 
                 T)))))))
-
 
 (defun is-regexp-body-mul (RE)
   (if (not (null (cdr RE)))
@@ -38,7 +34,6 @@
       T)))
 
 
-
 ;;;nfa-regexp-comp
 
 (defun nfa-regexp-comp (RE)
@@ -47,14 +42,12 @@
         (list (get-qiniziale Regexp)  Regexp (get-qfinale Regexp)))
     NIL))
 
-
 (defun crea_mossa (&optional carlettura)
   (let ((QIn (gensym))
         (QF (gensym)))
     (if (null carlettura)
         (list QIn 'epsilon QF)
       (list QIn carlettura QF))))
-
 
 (defun scompatta (RE)
   (if (atom RE)
@@ -85,7 +78,6 @@
                 (regexp_comp_plus (cdr RE))
               (list (crea_mossa RE)))))))))
 
-
 (defun regexp_comp_seq (RE)
   (if (null RE)
       NIL
@@ -100,13 +92,11 @@
 		       (get-qiniziale resto)))
                 resto)))))
 
-
 (defun regexp_comp_star (RE)
   (if (null RE)
       NIL
     (let ((Regexp (scompatta (car RE))))
       Regexp)))
-
 
 (defun regexp_comp_or (RE)
   (if (null RE)
@@ -130,18 +120,15 @@
 		 (list (get-qfinale primoElem) 'epsilon
 		       (get-qfinale resto))))))))
 
-
 (defun regexp_comp_plus (RE)
   (if (null RE)
       NIL
     (scompatta (list 'seq (car RE) (list 'star (car RE))))))
 
-
 (defun get-qiniziale (RE)
   (if (listp RE)
       (get-qiniziale (car RE))
     RE))
-
 
 (defun get-qfinale (RE)
   (if (listp RE)
@@ -166,80 +153,48 @@
       NIL)
     ))
 
-
 (defun nfa-test (Automa Input)
-
   (if (and (is-nfa Automa) (listp Input)) 
       (let ((QIn (first Automa))
-
 	    (Stati (second Automa))
-
 	    (QF (third Automa)))
-
 	(nodo-attuale QIn Stati QF Input))
     NIL))
 
 (defun nodo-attuale (QIn Automa QF Input)
-
   (if (and (string-equal QIn QF) (null Input))
-
       T
-
     (let ((stati-epsilon (passi-trovati-epsilon QIn Automa))
-
           (stati (passi-trovati-lettura QIn (first Input) Automa)))
 
       (if (equal (prova-stati stati-epsilon Automa QF Input) T)
-
           T
-
         (if (equal (prova-stati stati Automa QF (cdr Input)) T)
-
             T
-
           NIL)))))
 
-
 (defun prova-stati (stati Automa QF Input)
-
   (if (null stati)
-
       NIL
-
     (if (null (nodo-attuale (car stati) Automa QF Input))
-
         (prova-stati (cdr stati) Automa QF Input)
-
       T)))
 
 (defun passi-trovati-epsilon (QIn Automa)
 
   (if (null Automa)
-
       NIL
-
     (if (and (string-equal QIn (first (first Automa)))
-
              (equal 'epsilon (second (first Automa))))
-
         (append (list (third (first Automa)))
-
                 (passi-trovati-epsilon (third (first Automa)) Automa)
-
                 (passi-trovati-epsilon QIn (cdr Automa)))
-
       (passi-trovati-epsilon QIn (cdr Automa)))))
 
 (defun passi-trovati-lettura (QIn Lettura Automa)
-
   (if (null Automa)
-
       NIL
-
     (if (and (string-equal QIn (first (first Automa)))
-
              (equal Lettura (second (first Automa))))
-
         (append (list (third (first Automa))))
-
       (passi-trovati-lettura QIn Lettura (cdr Automa)))))
